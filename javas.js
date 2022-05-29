423050b5a32f7ad08a55d2adc5d4de1b87153dd1
metallica13 <metallica131313@mail.ru>
2022-05-29 23:04:03 +0300

    Ecosystem

--- javas.js
+++ javas.js
@@ -7,6 +7,6 @@
 //   window.scrollTo(0, 999999999999999);
 // };
 
-window.onload = function () {
-  window.scrollTo(0, document.body.scrollHeight);
-};
+// window.onload = function () {
+//   window.scrollTo(0, document.body.scrollHeight);
+// };
