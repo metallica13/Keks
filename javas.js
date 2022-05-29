4872e72720e8437481d81926e095922ce82ec8a2
metallica13 <metallica131313@mail.ru>
2022-05-29 21:58:16 +0300

    Leading Brands

--- javas.js
+++ javas.js
@@ -1,4 +1,12 @@
-window.scrollX = window.sessionStorage.scrollCache;
-window.addEventListener('scroll', () => {
-  window.sessionStorage.scrollCache = window.scrollX;
-});
+// window.scrollX = window.sessionStorage.scrollCache;
+// window.addEventListener('scroll', () => {
+//   window.sessionStorage.scrollCache = window.scrollX;
+// });
+
+// window.onload = function () {
+//   window.scrollTo(0, 999999999999999);
+// };
+
+window.onload = function () {
+  window.scrollTo(0, document.body.scrollHeight);
+};
